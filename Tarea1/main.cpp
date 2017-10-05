851e2aa9ffbabce7df4944b1c15cc7763d06db68
RmaxTwice <ravb2013@gmail.com>
2017-10-05 19:56:04 -0400

    Display TweakBar & Line drawing every click.
    - New function that draws a line between a current click coordinates
      and the previous ones.
    - Display the TweakBar called myBar.
    - Added change in coordinates every click.
    - Change of name for a couple functions.
    - IMPORTANT: Modified the orthogonal projection values to adjust to
      window resizing.

--- Tarea1/main.cpp
+++ Tarea1/main.cpp
@@ -6,10 +6,12 @@
 using namespace std;
 
 int gWidth, gHeight, window;
-//TwBar *sceneInterface;
+TwBar *myBar;
 bool box;
 string label;
 float color[3];
+int xi, yi, xf, yf;
+int first = 0;
 
 enum figure
 {
@@ -21,37 +23,61 @@ enum figure
 	TRIANGULO
 } select;
 
-void paint()
+void dibujarLinea(int x1, int y1, int x2, int y2)
+{
+	glColor3f(0.0, 0.0, 0.0);
+	glBegin(GL_LINES);
+		glVertex3f(x1, y1, 0.0);
+		glVertex3f(x2, y2, 0.0);
+	glEnd();
+}
+
+void display()
 {
 	glClear(GL_COLOR_BUFFER_BIT);
 	glColor3f(1.0, 1.0, 1.0);
-	glBegin(GL_POLYGON);
-		glVertex3f(0.25, 0.25, 0.0);
-		glVertex3f(0.75, 0.25, 0.0);
-		glVertex3f(0.75, 0.75, 0.0);
-		glVertex3f(0.25, 0.75, 0.0);
-	glEnd();
-	//TwDraw();
+
+	dibujarLinea(xi, yi, xf, yf);
+
+	TwDraw();
 	glFlush();
 }
 
 void initScene(int x, int y)
 {
-	glClearColor(1.0, 1.0, 1.0, 0.0);
+	glClearColor(1.0, 1.0, 1.0, 1.0);
 	glViewport(0, 0, x, y);
 	glMatrixMode(GL_PROJECTION);
 	glLoadIdentity();
-	glOrtho(0.0, 1.0, 0.0, 1.0, -1.0, 1.0);
-	//TwWindowSize(x, y);
+	glOrtho(0.0, x, y, 0.0, 0.0, 1.0);
+	TwWindowSize(x, y);
 }
 
 void mousePress(int button, int state, int x, int y)
 {
 	if (TwEventMouseButtonGLUT(button, state, x, y))
 		return;
-	if (button == GLUT_LEFT_BUTTON)
-		if (state == GLUT_DOWN)
-			cout << "Click" << endl;
+
+	if (button == GLUT_LEFT_BUTTON && state == GLUT_DOWN)
+		cout << "Click" << endl;
+
+	if (button == GLUT_LEFT_BUTTON && state == GLUT_DOWN)
+	{
+		switch (first)
+		{
+			case 0:
+				xi = x;
+				yi = y;
+				first = 1;
+				break;
+			case 1:
+				xf = x;
+				yf = y;
+				first = 0;
+				break;
+		}
+	}
+
 }
 
 void mouseMove(int x, int y)
@@ -80,6 +106,9 @@ void TW_CALL action1(void *clientData)
 	cout << "Presionaste el boton" << endl;
 }
 
+
+
+
 void initGlut(int argc, char **argv)
 {
 	glutInit(&argc, argv);
@@ -88,35 +117,35 @@ void initGlut(int argc, char **argv)
 	glutInitWindowSize(gWidth, gHeight);
 	window = glutCreateWindow("Example 1");
 	glutReshapeFunc(initScene);
-	glutDisplayFunc(paint);
+	glutDisplayFunc(display);
 	glutMotionFunc(mouseMove);
 	glutMouseFunc(mousePress);
 	glutKeyboardFunc(keyboard);
 }
-/*
+
 void initATB()
 {
 	TwInit(TW_OPENGL, NULL);
-	sceneInterface = TwNewBar("Interfaz");
+	myBar = TwNewBar("Interfaz");
 
 	TwDefine("Interfaz refresh = '0.0001f'");
 	TwDefine("Interfaz resizable = false");
 	TwDefine("Interfaz fontresizable = false");
 	TwDefine("Interfaz movable = true");
 	TwDefine("Interfaz visible = true");
-	TwDefine("Interfaz position = '20 20'");
-	TwDefine("Interfaz size = '220 320'");
+	TwDefine("Interfaz position = '5 5'");
+	TwDefine("Interfaz size = '200 250'");
 
 	TwEnumVal TypeFEV[] = { { NONE, "Niguno" },{ LINEA, "Linea" },{ CUADRILATERO, "Cudrilatero" },{ CIRCULO, "Circulo" },{ ELIPSE, "Elipse" },{ TRIANGULO, "Triangulo" } };
 	TwType TypesF;
 	TypesF = TwDefineEnum("comboBox", TypeFEV, 6);
-	TwAddVarRW(sceneInterface, "comboBox", TypesF, &select, NULL);
-	TwAddButton(sceneInterface, "button1", action1, NULL, "label = 'Boton 1'");
-	TwAddVarRO(sceneInterface, "label1", TW_TYPE_STDSTRING, &label, "label='label 1'");
-	TwAddVarRW(sceneInterface, "color1", TW_TYPE_COLOR3F, &color[0], "label = 'Color 1'");
-	TwAddVarRW(sceneInterface, "bool1", TW_TYPE_BOOLCPP, &box, "label = 'Booleano 1 '");
+	TwAddVarRW(myBar, "comboBox", TypesF, &select, NULL);
+	TwAddButton(myBar, "button1", action1, NULL, "label = 'Boton 1'");
+	TwAddVarRO(myBar, "label1", TW_TYPE_STDSTRING, &label, "label='label 1'");
+	TwAddVarRW(myBar, "color1", TW_TYPE_COLOR3F, &color[0], "label = 'Color 1'");
+	TwAddVarRW(myBar, "bool1", TW_TYPE_BOOLCPP, &box, "label = 'Booleano 1 '");
 }
-*/
+
 int main(int argc, char **argv)
 {
 	gWidth = 800;
@@ -126,7 +155,7 @@ int main(int argc, char **argv)
 	box = true;
 	color[0] = color[1] = color[2] = 0.5;
 	initGlut(argc, argv);
-	//initATB();
+	initATB();
 	glutMainLoop();
 	return 0;
 }
